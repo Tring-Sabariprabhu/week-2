465ac8d5ceb02aee9aed993260445a8c2e79b664
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-01-29 10:22:46 +0530

    Output Line changed

--- Profile.java
+++ Profile.java
@@ -19,7 +19,7 @@ class Profile{
     }
     public void validateName() {
         if (Username.trim().isEmpty()) {
-            throw new IllegalStateException("Username cannot be empty or null.");
+            throw new IllegalStateException("Error! Username cannot be empty or null");
         }
         System.out.println("Username is valid: " + Username);
     }
